609cf64adc1c2c89ae10c325b7c4a26906959144
Paolo van Reyk <paolo.vanreyk@gmail.com>
2023-06-06 18:02:47 +1000

    Added array and rudimentary version of generationFunction

---
+++ assests/script.js
@@ -0,0 +1,107 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+
+
+//Add Prompts
+
+
+function generatePassword() {
+    var passlength = 8,
+        charset = numericCharacters,
+        password = "";
+    for (var i = 0, n = charset.length; i < passlength; ++i) {
+        password += charset.substring(Math.floor(Math.random() * n));
+    }
+    return password;
+}
+
+// Add event listener to generate button
+//generateBtn.addEventListener("click", writePassword);
+
+//Call function upon click
+generatePassword()
