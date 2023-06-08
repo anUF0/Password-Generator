3834c3b1f35c382de78915464bd76d0b644d2c9b
Paolo van Reyk <paolo.vanreyk@gmail.com>
2023-06-08 12:47:16 +1000

    Fixed(?) promts and restructured selector code

--- assests/script.js
+++ assests/script.js
@@ -85,11 +85,10 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-
+const password = document.querySelector("#password");
 
 //Promts
-//TODO Make them do something
-var passlength = window.prompt("How long do want the password to be? (8-128 characters)");
+var chosenLength = window.prompt("How long do want the password to be? (8-128 characters, other inputs will be set to min/max)");
 var uppercaseConfirm = window.prompt("Should it include uppercase letters? (Y/N?)");
 var lowercaseConfirm = window.prompt("Should it include lowercase letter (Y/N?)");
 var numConfirm = window.prompt("Should it include numbers? (Y/N?)");
@@ -108,40 +107,57 @@ function getRandomSpChara() {
   return specialCharacters[Math.floor(Math.random() *  specialCharacters.length)];
 }
 
-//Listener and Promt Checker
-//generateBtn.addEventListener("click", () => {
-//  var length = passlength.value;
-//  var upper = uppercaseConfirm;
-//  var lower = lowercaseConfirm;
-//  var numbers = numConfirm;
-//  var spChara = spCharaConfirm;
-//  results.value =generatePassword(upper, lower, numbers, spChara, length)
-//});
+generateBtn.addEventListener("click", () => {
+ var passLength
+ 
+
+ if(chosenLength <  8){
+ passLength === 8;
+  } else if(chosenLength > 128){
+    chosenLength === 128;
+  } else {
+    chosenLength = passLength
+  }
+  if(uppercaseConfirm = 'y'){
+    uppercase = true;
+  }
+  if(lowercaseConfirm = 'y'){
+    lowercase= true;
+  }
+  if(numConfirm = 'y'){
+    numbers = true;
+  }
+  if(spCharaConfirm = 'y'){
+    spChara = true;
+  }
+  password.value = generatePassword(uppercase, lowercase, numbers, spChara, passLength)
+});
+
 
 //PasswordGenerator function
-function generatePassword() {
-       var tempPassword = "";
-       var y = true;
+function generatePassword(uppercase, lowercase, numbers, spChara, passLength) {
+       let tempPassword = "";
+       let variationsCount = [uppercase, lowercase, numbers, spChara].passLength;
 
-       for (let i = 0; i < length; i++){
-          if(uppercaseConfirm = y){
-            tempPassword += getRandomUpper
+       for (let i = 0; i < passLength; i+= variationsCount){
+          if(uppercase){
+            tempPassword += getRandomUpper();
         }
-        if(lowercaseConfirm = y){
-          tempPassword += getRandomLower
-      }  if(numConfirm = y){
-        tempPassword += getRandomNum
-    }  if(spCharaConfirm = y){
-      tempPassword += getRandomSpChara
+        if(lowercase){
+          tempPassword += getRandomLower();
+      }  if(numbers){
+        tempPassword += getRandomNum();
+    }  if(spChara){
+      tempPassword += getRandomSpChara();
   }
 }
 
-var password = tempPassword.slice(0, passlength);
+var password = tempPassword.slice(0, passLength);
 
 return password;
-
 }
 
-console.log(password)
+
+
 
 
